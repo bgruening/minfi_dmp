3ec15d7c7f56377dd22b9ef15ee51124f8fcb7a4
Ubuntu <ubuntu@ip-172-31-23-81.ec2.internal>
2018-02-22 14:16:19 +0000

    additional parameters

--- minfi_dmp.R
+++ minfi_dmp.R
@@ -7,16 +7,25 @@ args <- commandArgs(trailingOnly = TRUE)
 
 input1 = args[1]
 input2 = args[2]
-output = args[3]
+input3 = args[3]
+input4 = args[4]
+input5 = args[5]
+output1 = args[6]
 
 GRset <- get(load(input1))
 
 pheno <- read.csv(input2)
 
+type <- input3
+
+qCutoff <- as.numeric(input4)
+
+shrinkVar <- input5
+
 beta <- getBeta(GRset)
 
 pData  <- pData(GRset)
 
-dmp <- dmpFinder(beta, pheno$Phenotype, type = "continuous")
+dmp <- dmpFinder(beta, pheno$Phenotype, type = type, qCutoff = qCutoff, shrinkVar = shrinkVar)
 
-write.table(dmp, output)
+write.table(dmp, output1)
