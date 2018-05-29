bcd5493a1d9bf9ad84b91101a1b9ffcc8f74fd0c
kpbioteam <kpbioteam@gmail.com>
2018-05-29 14:59:40 +0000

    multiple input type should be added

--- minfi_dmp.R
+++ minfi_dmp.R
@@ -12,7 +12,11 @@ input4 = args[4]
 input5 = args[5]
 output1 = args[6]
 
-GRset <- get(load(input1))
+if (input1 = .rdata){
+set <- get(load(input1))
+}else{
+  set <- read.table(input1)
+}
 
 pheno <- read.table(input2,skip=1)
 
@@ -22,10 +26,6 @@ qCutoff <- as.numeric(input4)
 
 shrinkVar <- input5
 
-beta <- getBeta(GRset)
-
-pData  <- pData(GRset)
-
-dmp <- dmpFinder(beta, pheno$V2, type = type, qCutoff = qCutoff, shrinkVar = shrinkVar)
+dmp <- dmpFinder(set, pheno$V2, type = type, qCutoff = qCutoff, shrinkVar = shrinkVar)
 
 write.table(dmp, output1)
